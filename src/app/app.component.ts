ccf846f371a17cde83c33047fc2f5e3ced54af13
Timothy Ravor <timothyravor@live.nl>
2018-05-09 17:46:21 +0200

    Added navigation menu with hamburger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {NavigationService} from './navigation/navigation.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NavigationService]
 })
 export class AppComponent {
   title = 'app';
+  navigationService;
+  constructor(private navigationService: NavigationService) {
+    this.navigationService = navigationService;
+  }
+
+  @HostListener('click')
+  click() {
+    this.navigationService.toggle();
+  }
 }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, HostListener, Input, OnInit} from '@angular/core';
+import {NavigationService} from './navigation.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  providers: [NavigationService]
 })
 export class NavigationComponent implements OnInit {
+  @HostBinding('class.is-mobile')
+  mobile = false;
 
-  constructor() { }
+  constructor(private navigationService: NavigationService) {
+  }
 
   ngOnInit() {
+    this.navigationService.change.subscribe(isMobile => {
+      this.isMobile = isMobile;
+    });
   }
+
+
 
 }
