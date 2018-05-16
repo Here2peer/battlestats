aa72fe31b23398ccc4810b2ac5c731215e44bea3
Rick Huizing <rickhuizing@live.nl>
2018-05-16 17:54:31 +0200

    made it nicer

--- src/app/player-stats/player-stats.component.ts
+++ src/app/player-stats/player-stats.component.ts
@@ -19,6 +19,11 @@ export class PlayerStatsComponent implements AfterViewInit {
   statsMapping: any;
   gitMapping: any;
 
+  championIds: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22 , 23, 25, 26, 27, 28, 29, 30, 31, 38, 40, 41};
+  categories: {11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 20000,
+    21000, 22000, 23000, 24000, 25000, 26000, 27000, 28000, 40000};
+
   constructor(private playerStatsService: PlayerStatsService, private playerService: PlayerService) { }
 
   ngAfterViewInit() {
@@ -32,11 +37,13 @@ export class PlayerStatsComponent implements AfterViewInit {
         this.meta = data['meta'];
       }
     );
+
     this.playerStatsService.getStatsMapping().subscribe((data: any) => {
         console.log(data);
         this.statsMapping = data;
       }
     );
+
     this.playerStatsService.getGitMapping().subscribe((data: any) => {
         this.gitMapping = data.Mappings;
       }
