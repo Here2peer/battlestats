9e08f81b35c72a2324cf669a2b2525dd2883a8c4
Rick Huizing <rickhuizing@live.nl>
2018-05-28 19:32:35 +0200

    tournament jsonservice

--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { TournamentService} from './tournament.service';
 
 @Component({
   selector: 'app-tournament',
   templateUrl: './tournament.component.html',
   styleUrls: ['./tournament.component.css']
 })
+
 export class TournamentComponent implements OnInit {
   public activeTab = 'myTourneys';
-  constructor() { }
+
+  data: any;
+
+  constructor(private tournamentService: TournamentService) { }
+
   ngOnInit() {
+    this.tournamentService.getChampions().subscribe((data: any) => {
+      this.data = data;
+    });
   }
 
 
