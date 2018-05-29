0c3ab7a2aee83093b08945c41a5f61809049f57f
Rick Huizing <rickhuizing@live.nl>
2018-05-29 15:00:53 +0200

    cleanup

--- src/app/champions/champions.service.ts
+++ src/app/champions/champions.service.ts
@@ -8,7 +8,7 @@ export class ChampionsService {
   constructor(private http: HttpClient) {}
 
   getNewGameplay() {
-    return this.http.get('./assets/champions/newgameplay.json');
-    // return this.http.get('http://127.0.0.1:5000/gameplay');
+    // return this.http.get('./assets/champions/newgameplay.json');
+    return this.http.get('http://127.0.0.1:5000/gameplay');
   }
 }
