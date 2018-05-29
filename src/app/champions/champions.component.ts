ae3024711234c46361a08d3ff7100244ccbcedc1
Rick Huizing <rickhuizing@live.nl>
2018-05-29 14:53:31 +0200

    little cleanup

--- src/app/champions/champions.component.ts
+++ src/app/champions/champions.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {ChampionsService} from './champions.service';
-import {isBoolean} from 'util';
 
 @Component({
   selector: 'app-champions',
@@ -11,15 +10,6 @@ import {isBoolean} from 'util';
 export class ChampionsComponent implements OnInit {
 
   data: any;
-  catagory: any;
-  melee: any;
-  ranged: any;
-  support: any;
-  champions: any;
-  lore: boolean;
-
-  gameplay: any;
-  english: any;
   newgameplay: any;
 
   constructor(private championsService: ChampionsService) { }
@@ -34,25 +24,8 @@ export class ChampionsComponent implements OnInit {
 
 
   ngOnInit() {
-    this.championsService.getChampions().subscribe((data: any) => {
-        this.catagory = data;
-        this.champions = this.catagory['champions'];
-        this.melee = this.champions['melee'];
-        this.ranged = this.champions['ranged'];
-        this.support = this.champions['support'];
-      }
-    );
-    this.championsService.getGameplay().subscribe((data: any) => {
-        this.gameplay = data.characters;
-      }
-    );
     this.championsService.getNewGameplay().subscribe((data: any) => {
-        //console.log(data);
         this.newgameplay = data.characters;
-      }
-    );
-    this.championsService.getEnglishTexts().subscribe((data: any) => {
-        this.english = data;
       }
     );
   }
