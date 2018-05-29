03ff2a6849f0979ed5c57ce31af5c48607cf3b10
Timothy Ravor <timothyravor@live.nl>
2018-05-29 15:44:17 +0200

    Added steam login

--- src/app/steam/steam.component.ts
+++ src/app/steam/steam.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {SteamService} from './steam.service';
 
 
 @Component({
   selector: 'app-steam',
   templateUrl: './steam.component.html',
-  styleUrls: ['./steam.component.css']
+  styleUrls: ['./steam.component.css'],
+  providers: [SteamService]
 })
 export class SteamComponent implements OnInit {
 
+  steamuser: any;
 
-  constructor() {
+  constructor(private steamService: SteamService) {
   }
 
   ngOnInit() {
+    this.steamService.getSteamUser().subscribe((data: any) =>  {
+      this.steamuser = data;
+      if (this.steamuser == null) {
+        console.log(this.steamuser);
+      } else {
+        console.log('DATA FOUND!!!' + this.steamuser);
+      }
+    }
+    );
   }
 
 
