5a9f363efc7eafbf5320d0ef8a972265677988b5
Rick Huizing <rickhuizing@live.nl>
2018-05-31 20:17:49 +0200

    trying something

--- src/app/steam-auth/steam-auth.component.ts
+++ src/app/steam-auth/steam-auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-steam-auth',
@@ -9,13 +10,15 @@ import { ActivatedRoute} from '@angular/router';
 export class SteamAuthComponent implements OnInit {
   steamid: any;
   api = '47E81668C8A02D8BC4341E13C666BE0B';
-  constructor(private route: ActivatedRoute) { }
+  img_path: any;
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       if (params['steamid'] !== undefined) {
         this.steamid = params['steamid'];
       }});
+    this.img_path = this.http.get('https://127.0.0.1:5000/avatar');
   }
 
 }
