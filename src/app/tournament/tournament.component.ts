afb09cc56c040f2c0139487fe6602ed22d80b80e
Matthijs Boonstra <mp.boonstra@live.nl>
2018-06-01 12:39:35 +0200

    Added reading from .json for my tournaments

--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -10,16 +10,20 @@ import { TournamentService} from './tournament.service';
 export class TournamentComponent implements OnInit {
   public activeTab = 'myTourneys';
 
-  data: any;
+  tournamentData: any;
+  ownTourneyData: any;
 
   constructor(private tournamentService: TournamentService) { }
 
   ngOnInit() {
+    this.tournamentService.getTournamentList('00000001').subscribe((tdata: any) => {
+      console.log(tdata);
+      this.ownTourneyData = tdata;
+      });
     this.tournamentService.getTournamentData().subscribe((data: any) => {
       console.log(data);
-      this.data = data;
+      this.tournamentData = data;
     });
-    // TODO: Implement tournament builder function?
   }
 
 
--- src/app/tournament/tournament.service.ts
+++ src/app/tournament/tournament.service.ts
@@ -7,6 +7,11 @@ import {HttpClient} from '@angular/common/http';
 export class TournamentService {
   constructor(private http: HttpClient) {}
 
+  getTournamentList(userID) {
+    userID = '00000001';
+    return this.http.get('./assets/tournaments/tournaments_p' + userID + '.json');
+  }
+
   getTournamentData() {
     return this.http.get('./assets/tournaments/tournament_00000002.json');
   }
