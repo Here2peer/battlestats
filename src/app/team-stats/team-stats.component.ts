c6c9a53806f8d19fec7d84ea2832125d66f38842
Timothy Ravor <timothyravor@live.nl>
2018-07-02 13:22:23 +0200

    Sort added and minor changes

--- src/app/team-stats/team-stats.component.ts
+++ src/app/team-stats/team-stats.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {TeamStatsService} from './team-stats.service';
 import { PlayerService } from '../player-services/player.service';
 import {ActivatedRoute} from '@angular/router';
+import {Sort} from '@angular/material';
 
 @Component({
   selector: 'app-team-stats',
@@ -35,6 +36,7 @@ export class TeamStatsComponent implements OnInit {
     this.teamStatsService.getTeamStats(player_name).subscribe((data: any) => {
       if (!data['errors']) {
         this.teamstats = data;
+        console.log(this.teamstats);
       } else {
         this.error = data['errors'][0]['title'];
       }
@@ -46,5 +48,32 @@ export class TeamStatsComponent implements OnInit {
         this.customstats = this.attribute['customstats'];
       }
     );
+  }
+
+  sortData(sort: Sort) {
+    const data = this.teamstats.slice();
+    if (!sort.active || sort.direction === '') {
+      this.teamstats = data;
+      return;
+    }
+
+    function compare(a, b, isAsc) {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
+
+
+    this.teamstats = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Games left': return compare(a.attributes.stats.placementGamesLeft, b.attributes.stats.placementGamesLeft, isAsc);
+        case 'Wins': return compare(a.attributes.stats.wins, b.attributes.stats.wins, isAsc);
+        case 'Losses': return compare(a.attributes.stats.losses, b.attributes.stats.losses, isAsc);
+        case 'League': return compare(a.attributes.stats.league, b.attributes.stats.league, isAsc);
+        case 'Division': return compare(a.attributes.stats.division, b.attributes.stats.division, isAsc);
+        case 'Top division': return compare(a.attributes.stats.topDivision, b.attributes.stats.topDivision, isAsc);
+        case 'Top League': return compare(a.attributes.stats.topLeague, b.attributes.stats.topLeague, isAsc);
+        default: return 0;
+      }
+    });
   }
 }
